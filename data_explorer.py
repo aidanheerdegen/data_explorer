af89dade19eb24a4df9d8eeb2aed5982dfaee120
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-07-15 10:08:04 +1000

    Working version but with some layout debugging stuff

--- data_explorer.py
+++ data_explorer.py
@@ -4,18 +4,21 @@ import cosima_cookbook as cc
 import ipywidgets as widgets
 from ipywidgets import Button, VBox, HBox, Label, Layout, Select
 from ipywidgets import SelectMultiple, Text, Textarea, Checkbox
-from ipywidgets import interact, interact_manual
+from ipywidgets import interact, interact_manual, AppLayout
 import ipywidgets as wid
 
 import pandas as pd
 
+from cosima_cookbook.database import CFVariable, NCFile, NCExperiment, NCVar
+
+from sqlalchemy import func
 class DatabaseExtension:
 
     session = None
     experiments = None
     keywords = None
     variables = None
-    expt_keyword_map = None
+    expt_variable_map = None
     restart_variables = None
     
     def __init__(self, session=None):
@@ -24,34 +27,45 @@ class DatabaseExtension:
         self.session              = session
         self.experiments          = cc.querying.get_experiments(session, all=True)
         self.keywords             = cc.querying.get_keywords(session)
-        self.expt_keyword_map     = self.experiment_variable_map()
+        self.expt_variable_map     = self.experiment_variable_map()
         self.variables            = self.unique_variable_list()
-        self.restart_variables    = self.get_restart_variables()
-    
-    def get_restart_variables(self):
-        """
-        Make a pandas table with experiment as the index and columns
-        of name and long_name
-        """
-        allvars = pd.concat([cc.querying.get_variables(self.session, expt)
-                     for expt in self.experiments.experiment], keys=self.experiments.experiment)
-        return allvars[allvars.ncfile.str.contains('restart')][['name','long_name']].reset_index(drop=True).drop_duplicates()
 
     def experiment_variable_map(self):
         """
         Make a pandas table with experiment as the index and columns
-        of name and long_name
+        of name, long_name and restart flag.
+
+        Also make lists of unique name/long_name 
         """
-        return pd.concat([cc.querying.get_variables(self.session, expt)[['name', 'long_name']] 
+
+        allvars = pd.concat([self.get_variables(expt)
                      for expt in self.experiments.experiment], keys=self.experiments.experiment)
+
+        # Create a new column to flag if variable is from a restart directory
+        allvars['restart'] = allvars.ncfile.str.contains('restart')
+
+        # Create a new column to flag if variable has units which match a number of criteria
+        # that indicated it is a coordinate
+        allvars = allvars.assign(coordinate=(allvars.units.str.contains('degrees', na=False) 
+                                            | allvars.units.str.contains('since', na=False) 
+                                            | allvars.units.str.match('^radians$', na=False)
+                                            | allvars.units.str.startswith('days', na=False)))  # legit units: %/day, day of year
+
+        return allvars[['name', 'long_name', 'restart', 'coordinate']]
 
     def unique_variable_list(self):
         """
         Extract a list of all variable name/long_name pairs from the experiment
         keyword map
         """
-        return self.expt_keyword_map.reset_index(drop=True).drop_duplicates()
+        return self.expt_variable_map.reset_index(drop=True).drop_duplicates()
         
+    def get_restart_variables(self):
+        """
+        Return a table of all variables (name/long_name) from a restart directory
+        """
+        return self.variables[self.variables.restart]
+
     def keyword_filter(self, keywords):
         """
         Return a list of experiments matching *all* of the supplied keywords
@@ -65,12 +79,44 @@ class DatabaseExtension:
         expts = []
         for v in variables:
             expts.append(
-                set(self.expt_keyword_map[self.expt_keyword_map.name == v].reset_index()['experiment'])
+                set(self.expt_variable_map[self.expt_variable_map.name == v].reset_index()['experiment'])
             )
         return set.intersection(*expts)
     
     def get_experiment(self, experiment):
         return self.experiments[self.experiments['experiment'] == experiment]
+
+    # Return more metadata than get_variables from cosima-cookbook
+    def get_variables(self, experiment, frequency=None):
+        """
+        Returns a DataFrame of variables for a given experiment and optionally
+        a given diagnostic frequency.
+        """
+
+        q = (self.session
+            .query(CFVariable.name,
+                    CFVariable.long_name,
+                    CFVariable.standard_name,
+                    CFVariable.units,
+                    NCFile.frequency,
+                    NCFile.ncfile,
+                    func.count(NCFile.ncfile).label('# ncfiles'),
+                    func.min(NCFile.time_start).label('time_start'),
+                    func.max(NCFile.time_end).label('time_end'))
+            .join(NCFile.experiment)
+            .join(NCFile.ncvars)
+            .join(NCVar.variable)
+            .filter(NCExperiment.experiment == experiment)
+            .order_by(NCFile.frequency,
+                    CFVariable.name,
+                    NCFile.time_start,
+                    NCFile.ncfile)
+            .group_by(CFVariable.name, NCFile.frequency))
+
+        if frequency is not None:
+            q = q.filter(NCFile.frequency == frequency)
+
+        return pd.DataFrame(q)
 
 def DatabaseExplorer(session=None, de=None):
 
@@ -88,18 +134,19 @@ def DatabaseExplorer(session=None, de=None):
         options=de.experiments.experiment,
         rows=20,
         # description='Experiments:',
-        layout={'width': 'initial'},
+        layout={'width': '100%'},
         disabled=False
     )
 
-    box_layout = Layout(position='left', width='initial', border= '0px solid black')
+    box_layout = Layout(position='left', width='initial', border= '1px solid black')
 
     # Keyword filtering element
     filter_widget = VBox(layout={'overflow': 'auto', 
                                  'width': 'initial', 
-                                 'border': '0px solid black'})
+                                 'border': '1px solid black'})
     keywords_checkboxes = [Checkbox(description=str(k), 
                                     value=False, 
+                                    indent=False,
                                     layout=box_layout) for k in de.keywords]
     filter_widget.children = keywords_checkboxes
 
@@ -144,8 +191,8 @@ def DatabaseExplorer(session=None, de=None):
         options=sorted(de.variables.name),
         rows=20,
         # description='Experiments:',
-        layout={'width': 'initial'},
-        disabled=False
+        layout={'width': '100%'},
+        disabled=False,
     )
 
     # Experiment information elements
@@ -153,26 +200,50 @@ def DatabaseExplorer(session=None, de=None):
         value='',
         placeholder='Experiment description',
         description='Description',
-        layout={'height': '100%', 'width': '100%'}
+        layout={'height': '100%', 'width': '100%', 'border': '1px solid black'}
     )
     expt_notes = Textarea(
         value='',
         placeholder='Experiment notes',
         description='Notes',
-        layout={'height': '100%', 'width': '100%'},
+        layout={'height': '100%', 'width': '100%', 'border': '1px solid black'},
         disabled=False
     )
     
     # Variable filtering elements
     var_filter_coords = Checkbox(
         value = True,
+        indent=False,
         description='Hide coordinates',
+        layout={'width': '100%', 'border': '1px solid black'},
     )
     var_filter_restarts = Checkbox(
         value = True,
+        indent=False,
         description='Hide restarts',
+        layout={'width': '100%', 'border': '1px solid black'},
     )
 
+    # Reset button
+    def reset_variables(b):
+        var_filter_selector.value = ()
+
+    reset_var_button = Button(
+        description='Reset',
+        disabled=False,
+        layout={'width': '50%', 'align': 'center'},
+        tooltip='Click to reset variable selection'
+    )
+    reset_var_button.on_click(reset_variables)
+        
+    # Element for filtering experiments by variables
+    var_filter_selector = SelectMultiple(
+        options=sorted(de.variables.name),
+        rows=20,
+        # description='Experiments:',
+        layout={'width': '100%'},
+        disabled=False
+    )
     variables = None
     
     def expt_eventhandler(selector):
@@ -184,9 +255,7 @@ def DatabaseExplorer(session=None, de=None):
         if selector.new is None:
             return
 
-        expt = de.get_experiment(selector.new)
-        expt_description.value = return_value_or_empty(expt.description.values[0])
-        expt_notes.value       = return_value_or_empty(expt.notes.values[0])
+        show_experiment_information()
 
     expt_selector.observe(expt_eventhandler, names='value')
     
@@ -195,53 +264,126 @@ def DatabaseExplorer(session=None, de=None):
         When experiment is selected populate the experiment information
         elements
         """
-        options = set(de.variables.name)
-        if var_filter_restarts.value:
-            # Remove all variables with restart in ncfile field
-            options.difference_update(de.restart_variables)
+        # Set up a mask with all true values
+        mask = de.variables.name.ne('')
 
-        var_filter_selector.options = sorted(options)
+        # Filter out restarts and coordinates if checkboxes selected 
+        if var_filter_restarts.value:
+            mask = mask & (de.variables['restart'] != var_filter_restarts.value)
+        if var_filter_coords.value:
+            mask = mask & (de.variables['coordinate'] != var_filter_coords.value)
+
+        # Mask options
+        var_filter_selector.options = sorted(de.variables[mask].name, key=str.casefold)
 
     filter_restart_eventhandler(None)
 
     var_filter_restarts.observe(filter_restart_eventhandler, names='value')
+    var_filter_coords.observe(filter_restart_eventhandler, names='value')
 
+    def load_experiment(b):
+        """
+        Open an Experiment Explorer UI with selected experiment
+        """
+        if expt_selector.value is not None:
+            ExperimentExplorer(expt_selector.value, session, de)
+
+    def show_experiment_information():
+
+        experiment = expt_selector.value
+        expt = de.get_experiment(experiment)
+
+        expt_info.value="""
+        <h4>Experiment: {experiment}</h4>
+
+        <b>Description:</b> {description}
+
+        <b>Notes:</b> {notes}
+
+        <b>Contact:</b> {contact} <{email}>
+
+        <b>Number of files::</b> {nfiles}
+
+        <b>Created::</b> {created}
+        """.format(
+                   experiment=experiment,
+                   description=return_value_or_empty(expt.description.values[0]),
+                   notes=return_value_or_empty(expt.notes.values[0]),
+                   contact=return_value_or_empty(expt.contact.values[0]),
+                   email=return_value_or_empty(expt.email.values[0]),
+                   nfiles=return_value_or_empty(expt.ncfiles.values[0]),
+                   created=return_value_or_empty(expt.created.values[0]),
+                   )
+        # ['experiment', 'contact', 'email', 'created', 'description', 'notes', 'root_dir', 'ncfiles']
+    expt_info = widgets.HTML(
+        value='',
+        description='Experiment Information:',
+    )
+
+    load_button = widgets.Button(
+        description='Load Experiment',
+        disabled=False,
+        layout={'width': '20%', 'align': 'center'},
+        button_style='', # 'success', 'info', 'warning', 'danger' or ''
+        tooltip='Click to load experiment'
+        #icon='check'
+    )
+    load_button.on_click(load_experiment)
+
+    header = widgets.HTML(
+        value="""
+        <h3>Database Explorer</h3>
+
+        <p>Select keywords and/or variables and push 'Filter' to show only 
+        matching experiments. Use option or shift key to select multiple variables</p>
+
+        <p>Select an experiemt to show more detailed information where available.
+        With an experiment selected push 'Load' to open an Experiment Explorer gui,
+        or copy the experiment name and use in your own code.
+        </p>
+        """,
+        description='',
+    )
+    
+    display(header)
     selectors = HBox(
                     [
                       VBox([
                            Label(value="Experiments:"), 
                            expt_selector,
                            ],
-                           layout={'padding': '10px', 'border': '0px solid grey'}
+                           layout={'padding': '10px', 'border': '1px solid grey'}
                            ),
                       VBox([
                             Label(value="Filter by keyword:"), 
                             filter_widget, 
-                            filter_button
+                            filter_button,
                             ],
-                            layout={'padding': '10px', 'border': '0px solid grey'}
+                            layout={'padding': '10px', 'border': '1px solid grey'}
                           ),
                       VBox([
                             Label(value="Filter by variable:"), 
                             var_filter_selector],
-                            layout={'padding': '10px', 'border': '0px solid grey'}
+                            layout={'padding': '10px', 'border': '1px solid grey'}
                            ),
                       VBox([
                             var_filter_coords,
-                            var_filter_restarts
+                            var_filter_restarts, 
+                            reset_var_button, 
                             ],
                             layout={'border': '0px solid grey'}
                            )
-                    ], layout={'width': '100%', 'padding': '10px', 'border': '0px solid black'}
+                    ], layout={'width': '100%', 'padding': '10px', 'border': '1px solid black'}
                     )
     display(selectors)
                               
-    info = widgets.HBox([expt_description,expt_notes],
+    info = widgets.HBox([expt_info, ],
                          layout={'width': '100%', 'height': '200px', 'padding': '10px', 'border': '0px solid black'}
                    )
     #display(output)
     display(info)
-    # display(var_selector)
+    load_box = widgets.HBox([load_button,])
+    display(load_box)
 
 def ExperimentExplorer(experiment, session=None, de=None):
 
@@ -251,7 +393,7 @@ def ExperimentExplorer(experiment, session=None, de=None):
             return ''
         else:
             return value
-    
+
     if session is None:
         session = cc.database.create_session()
 
@@ -368,26 +510,28 @@ def ExperimentExplorer(experiment, session=None, de=None):
     
     display(header)
     
+    data_box = widgets.HTML()
+
     # Data loading button
     def load_data(b):
-        global ds
         varname = var_selector.value
         (start_time, end_time) = var_daterange.value
-#        print("""ds = cc.querying.getvar({expt}, 
-#                                {var},
-#                                session,
-#                                start_time={start},
-#                                end_time={end})
-#        """.format(expt=expt_selector.value, 
-#                   var=var_selector.value, 
-#                   start=str(start_time),
-#                   end=str(endt_time))
+        print("""ds = cc.querying.getvar('{expt}', 
+                               '{var}',
+                               session,
+                               start_time='{start}',
+                               end_time='{end}')
+       """.format(expt=expt_selector.value, 
+                  var=var_selector.value,
+                  start=str(start_time),
+                  end=str(end_time)))
         ds = cc.querying.getvar(expt_selector.value, 
                                 var_selector.value, 
                                 session, 
                                 start_time=str(start_time),
                                 end_time=str(end_time))
-        print(repr(ds))
+        data_box.value=ds._repr_html_()
+
     load_button = widgets.Button(
         description='Load',
         disabled=False,
@@ -408,3 +552,9 @@ def ExperimentExplorer(experiment, session=None, de=None):
                         layout=box_layout
                         )
     display(widgets.HBox([widgets.VBox([var_search, var_selector]), var_info], layout=box_layout))
+    display(data_box)
+
+
+def VariableExplorer(ds):
+
+    ds.hvplot.quadmesh(datashade=True)
